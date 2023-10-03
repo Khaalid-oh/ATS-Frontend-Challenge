0e81072395ef4e56da02e5b1c9393ca4562d8eba
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2023-10-03 17:28:21 +0100

    update dashboard icon

--- ats-app/src/app/components/organisms/sideBar.tsx
+++ ats-app/src/app/components/organisms/sideBar.tsx
@@ -3,17 +3,20 @@ import { MenuOutlined } from "@ant-design/icons";
 import { HomeOutlined } from "@ant-design/icons";
 import { ContainerOutlined } from "@ant-design/icons";
 import Dp from "../atoms/dp";
+import DashboardIcon from "@/app/resources/DashboardIcon";
 
 function SideBar() {
   return (
     <div className="h-[300vh] w-20 shadow-lg flex items-center flex-col space-y-[520px] py-8">
-      <div className="flex flex-col gap-16">
+      <div className="flex flex-col items-center justify-center gap-16">
         <div>
           <MenuOutlined style={{ fontSize: "19px" }} />
         </div>
         <div className="flex flex-col gap-8">
           <HomeOutlined style={{ fontSize: "19px" }} />
-          <ContainerOutlined style={{ fontSize: "19px" }} />
+          <div className="translate-x-1">
+            <DashboardIcon />
+          </div>
         </div>
       </div>
       <div className="">
